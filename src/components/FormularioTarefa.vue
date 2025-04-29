aecd40ab2542684b232e21452eb21cb0fe04425c
birajnior <96500363+birajnior@users.noreply.github.com>
2025-04-29 17:45:33 -0300

    feat: Adicionando logica do cronometo

--- src/components/FormularioTarefa.vue
+++ src/components/FormularioTarefa.vue
@@ -17,15 +17,15 @@
           class="is-flex is-aling-items-center is-justify-content-space-between"
         >
           <section>
-            <strong>00:00:00</strong>
+            <strong>{{ tempoDecorrido }}</strong>
           </section>
-          <button class="button">
+          <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
             <span>Play</span>
           </button>
-          <button class="button">
+          <button class="button" @click="finalizar">
             <span class="icon">
               <i class="fas fa-stop"></i>
             </span>
@@ -42,5 +42,28 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "FormularioTarefa",
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+    };
+  },
+  computed: {
+    tempoDecorrido(): string {
+      return new Date(this.tempoEmSegundos * 1000)
+        .toISOString()
+        .substring(11,19);
+    },
+  },
+  methods: {
+    iniciar() {
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+         },
+    finalizar() {
+      clearInterval(this.cronometro)
+    },
+  },
 });
 </script>
